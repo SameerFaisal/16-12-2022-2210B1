f4e659a0462e1ec1521be7ce1c7465e63f105006
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2022-12-16 11:06:29 +0500

    16-12-2022

---
+++ 16-12-2022/app.js
@@ -0,0 +1,72 @@
+// var data=["Talha","Ali","Wasay"]
+// var dataCopy=data.slice(1,3)
+
+// data.splice(1, 1)
+
+// data.push("Saad")
+// data.pop()
+// data.push("Wahaj")
+// data.push("Hammad")
+// data.pop()
+// data.pop()
+
+// data.unshift("Sameer")
+
+// data.splice(0,0,"Haider","Amir","Laiba")
+
+
+
+// var value=+prompt("enter how many values you want to add?")
+
+// for(j=0;j<value;j++){
+// data.push("data");
+// }
+
+// var value=+prompt("enter how many values you want to delete?")
+
+// for(j=0;j<value;j++){
+// data.pop();
+// }
+
+// var studentData=["Ali",23,"someone@example.com","2210B1",["Cooking","Stitching",["Cake","Cookies"],"Sports"]]
+
+// document.write(studentData[4][2][1])
+
+// var data=[[14350,10500,1500,26350],[34350,4050,1000,39400]]
+
+// document.write(data)
+
+
+
+// for(var i=0;i<data.length;i++){
+//     document.write(data[i]+"<br>")
+// }
+// document.write("<h1>Original Data Array</h1>")
+// for(var i=0;i<studentData.length;i++){
+//     document.write(studentData[i]+"<br>")
+// }
+
+
+// var sentence="I love my country"
+
+
+// var copy=sentence.slice(5,12)
+// document.write(copy)
+
+// for(var i=0;i<sentence.length;i++){
+//     document.write(sentence[i]+"<br>")
+// }
+
+// document.write("<h1>Copy Array</h1>")
+// for(var i=0;i<dataCopy.length;i++){
+//     document.write(dataCopy[i]+"<br>")
+// }
+
+
+// var arr=[9,25,21,1,0,45,5]
+
+// for(var i in arr){
+//     document.write(arr[i]+"<br>")
+// }
+
+
